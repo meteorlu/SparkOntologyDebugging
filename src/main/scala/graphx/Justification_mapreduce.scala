22c56d5473104910bfe9a176bd19a3a5250e930e
meteorlu <429763641@qq.com>
2017-12-11 16:58:05 +0800

    修正mapreduce的求解陷入死循环问题

--- src/main/scala/graphx/Justification_mapreduce.scala
+++ src/main/scala/graphx/Justification_mapreduce.scala
@@ -13,6 +13,7 @@ import utils.Triple
 
 import scala.collection.mutable
 import scala.collection.mutable.{ArrayBuffer, ListBuffer, Set}
+import scala.io.Source
 
 /**
   * Created by user on 2017/5/25.
@@ -31,52 +32,74 @@ object Justification_mapreduce {
   var targetNum: Long = 0L
   var accum:LongAccumulator = null
   var resultJustification:Broadcast[Set[Set[Triple]]] = null
+  var justifications:Set[Set[Triple]] =Set()
+  var resultSet:Set[(Array[String],Set[Set[Triple]])] = Set()
   var resultSize:Int = 0
   var theSameTime:Int = 0
   def main(args: Array[String]) {
     var i: Int = 0
-    for (i <- 0 to args.length - 1) {
-      if (args(i).equals("--subject"))
-        sub = args(i + 1).toLong
-      if (args(i).equals("--predicate"))
-        pre = args(i + 1).toLong
-      if (args(i).equals("--object"))
-        obj = args(i + 1).toLong
-    }
-    if (sub == 0L && pre == 0L && obj == 0L) {
-      println("please input args --subject --predicate --object")
-      return
-    }
-    var triple = new Triple(sub, pre, obj, false)
-    var explanation: Set[Triple] = Set(triple)
+//    for (i <- 0 to args.length - 1) {
+//      if (args(i).equals("--subject"))
+//        sub = args(i + 1).toLong
+//      if (args(i).equals("--predicate"))
+//        pre = args(i + 1).toLong
+//      if (args(i).equals("--object"))
+//        obj = args(i + 1).toLong
+//    }
+//    if (sub == 0L && pre == 0L && obj == 0L) {
+//      println("please input args --subject --predicate --object")
+//      return
+//    }
+//    var triple = new Triple(sub, pre, obj, false)
+//    var target = (sub, pre, obj)
+//    var explanation: Set[Triple] = Set(triple)
     //屏蔽日志
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
     Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
 
-    // 配置spark，cassandra的ip，这里都是本机
-    val SparkMasterHost = "local"
-    val CassandraHost = "127.0.0.1"
-    // Tell Spark the address of one Cassandra node:
-    val conf = new SparkConf(true)
-      .set("spark.cassandra.connection.host", CassandraHost)
-      .set("spark.cleaner.ttl", "3600")
-      .setMaster(SparkMasterHost)
-      .setAppName("CassandraTestApp")
-
-    // Connect to the Spark cluster:
-    lazy val sc = new SparkContext(conf)
+//    // 配置spark，cassandra的ip，这里都是本机
+//    val SparkMasterHost = "local"
+//    val CassandraHost = "127.0.0.1"
+//    // Tell Spark the address of one Cassandra node:
+//    val conf = new SparkConf(true)
+//      .set("spark.cassandra.connection.host", CassandraHost)
+//      .set("spark.cleaner.ttl", "3600")
+//      .setMaster(SparkMasterHost)
+//      .setAppName("CassandraTestApp")
+//
+//    // Connect to the Spark cluster:
+//    lazy val sc = new SparkContext(conf)
+    //设置运行环境
+    val conf = new SparkConf()
+      .setAppName("SimpleGraphX")
+      .setMaster("local")
+    var sc = new SparkContext(conf)
     //初始化累加器
     accum = sc.longAccumulator("Accumulator")
     resultJustification = sc.broadcast(Set())
-    //预处理脚本,连接的时候就执行这些
-    CassandraConnector(conf)
-    //get data from cassandra
-    start = System.nanoTime()
     try {
       CassandraDB.connect()
-      //map 过程
-      var rddInit: RDD[(Triple, Set[Triple])] = sc.makeRDD(Seq((triple, explanation)))
-      findJustifications(rddInit)
+      var fileSource = Source.fromFile("inputs.txt")
+      var dataRows:Set[Tuple3[Long,Long,Long]] = Set()
+      for(line <- fileSource.getLines) {
+        var strs = line.split("\t")
+        sub = strs(0).toLong
+        pre = strs(1).toLong
+        obj = strs(2).toLong
+        var triple = new Triple(sub, pre, obj, false)
+        var target = (sub, pre, obj)
+        println("triple:"+triple)
+        var explanation: Set[Triple] = Set(triple)
+        initParams()
+        //初始化
+        start = System.nanoTime()
+        var rddInit: RDD[(Triple, Set[Triple])] = sc.makeRDD(Seq((triple, explanation)))
+        findJustifications(rddInit)
+        end = System.nanoTime()
+        var results = Array(target.toString(), (resultJustification.value.size).toString, ((end - start) / 1000000).toString)
+        resultSet.add((results, resultJustification.value))
+      }
+      writeToResults(resultSet)
     } catch {
       case ex: Exception => ex.printStackTrace()
     } finally {
@@ -84,22 +107,35 @@ object Justification_mapreduce {
     }
     sc.stop()
   }
-
-  //讲结果保存到文本文件
-  def writeToResults(results: Set[ArrayBuffer[String]]) = {
-    var writer = new PrintWriter(new File("D:\\results.txt"))
-    results.foreach(r => writer.println(r))
+  def initParams(): Unit ={
+    resultJustification.value.clear()
+    resultSize = 0
+    theSameTime = 0
+  }
+  /**
+    * 将结果保存到文本文件
+    * @param results
+    */
+  def writeToResults(results:Set[(Array[String],Set[Set[Triple]])]) =  {
+    var writer = new PrintWriter(new File("resultsMapReduce.txt"))
+    results.foreach(r => {
+      writer.println(r._1.apply(0)+"\t"+r._1.apply(1)+"\t"+r._1.apply(2))
+      //      writer.println("Justifications:")
+      //      r._2.foreach(r => writer.println(r))
+    })
     writer.close()
   }
 
   //求取辩解集合
   def findJustifications(rddInit: RDD[(Triple, Set[Triple])]): Unit = {
+//    println("*************************")
+//    rddInit.collect().foreach(println(_))
 //    rddInit.foreach( x=>println("三元组："+x._1+"******* 辩解集和："+x._2))
-    var mapRdd: RDD[(Set[Triple],Triple, Int)] = rddInit.flatMap(x => {
+    var mapRdd: RDD[(Set[Triple], Int)] = rddInit.flatMap(x => {
       var triple:Triple = x._1
       var explanation: Set[Triple] = x._2.clone()
       //输出的结果集合
-      var results: Set[(Set[Triple],Triple, Int)] = Set()
+      var results: Array[(Set[Triple], Int)] = Array()
       //创建一个不包括自身的集合
       val toExtendExplanations: Set[Triple] = x._2.clone()
       toExtendExplanations.remove(triple)
@@ -128,45 +164,68 @@ object Justification_mapreduce {
       }
       if (tracingEntries.isEmpty) {
 //        println("【到头了】："+triple)
-        results.add((explanation,triple,1))
+        results = Array.concat(results,Array((explanation,1)))
       } else {
-//        println("有前置条件tracingEntries："+tracingEntries)
+//        println("有超边集合tracingEntries："+tracingEntries)
 //        println("前置条件如下：")
         tracingEntries.foreach(tracingEntry => {
           var tracedTriples: Set[Triple] = utils.Util.tracing(tracingEntry)
-//          println(tracingEntry+"："+tracedTriples)
-          if(!tracedTriples.contains(triple)){
+//          println("超边tracingEntry："+tracingEntry)
+//          println("前提集合："+tracedTriples)
+          if(tracedTriples.nonEmpty && includeSetTriples(x._2,tracedTriples)){
+            println("include tracingEntry:"+tracingEntry)
+            println("include tracedTriples:"+tracedTriples)
+            println("include x._2:"+x._2)
+          }
+//          println("toExtendExplanations:"+toExtendExplanations)
+//          println("tracedTriples:"+tracedTriples)
+//          println(tracingEntry"："+tracedTriples)
+          if(!tracedTriples.contains(triple) ){
             var newExplanation: Set[Triple] = toExtendExplanations.clone()
+            var allExist = true
             tracedTriples.foreach(tracedTriple => {
               if(!newExplanation.exists(t => t.equals(tracedTriple))){
                 newExplanation.add(tracedTriple)
-              }
+              }else allExist = false
             })
-            if(tracedTriples.isEmpty){
-              results.add((explanation,triple,1))
-            }else if(toExtendExplanations.size == newExplanation.size) {
-              results.add((explanation,triple,1))
+//            if(allExist && tracedTriples.nonEmpty){
+//                          println("triple:"+triple)
+//                          println("tracedTriples:"+tracedTriples)
+//                          println("explanation:"+x._2)
+//                        }
+            if(tracedTriples.isEmpty || !allExist){
+              results = Array.concat(results,Array((explanation,1)))
+//            }else if(toExtendExplanations.size == newExplanation.size) {
+//              results.add((explanation,triple,1))
             }else{
-              results.add((newExplanation,tracingEntry,0))
+              results = Array.concat(results,Array((newExplanation,0)))
             }
+          }else{
+            results = Array.concat(results,Array((explanation,1)))
           }
         })
       }
       results
     })
-//    //打印map结果
+//    println("reduceRdd:")
+//    mapRdd.collect().foreach(println(_))
+    //打印map结果
 //    println("reduceByKey之前：" + mapRdd.collect().size)
-//    mapRdd.sortBy(x=>x._1.toString()).foreach(x => println("Value:"+x._3+"***** triple:"+x._2+" *****  "+"Key:"+x._1))
+//    mapRdd.sortBy(x=>x._1.toString()).foreach(x => println("Value:"+x._2+"*****        Key:"+x._1))
 //    //进行reduce过程
-//    var redRdd = mapRdd.map(x => (x._1,x._3)).reduceByKey(_+_)
+//    var redRdd = mapRdd.reduceByKey(_+_)
 //    println("reduceByKey之后：" + redRdd.collect().size)
-//    redRdd.sortBy(x=>x._1.toString()).foreach(x => println("Value:"+x._2+" *****  "+"Key:"+x._1))
-    var reduceRdd = mapRdd.map(x => (x._1,x._3)).reduceByKey(_+_).flatMap(x => {
+//    redRdd.sortBy(x=>x._1.toString()).foreach(x => println("Value:"+x._2+" **size:"+x._1.size+"*** Key:"+x._1))
+    var justifications = resultJustification.value
+    var reduceRdd = mapRdd.reduceByKey(_+_).flatMap(x => {
       var results: Set[(Triple, Set[Triple])] = Set()
       if (x._2 == x._1.size) {
         //保存一个辩解结合
         if(!resultJustification.value.exists(result => compareSetTriples(result,x._1))){
           resultJustification.value.add(x._1)
+//          println("newone:"+x._1)
+        }else{
+//          println("exists:"+x._1)
         }
 //        var writeTxt = new ArrayBuffer[String]()
 //        resultJustification.foreach(just => {
@@ -174,26 +233,24 @@ object Justification_mapreduce {
 //        })
       }else if(x._2 == 0) {
         x._1.foreach(triple => {
-          results.add((triple, x._1))
+            results.add((triple, x._1))
         })
       }
       results
     })
+
     accum.add(1L)
-//    println(accum.value)
-    //判断辩解集和是否增加
-    if(resultSize == resultJustification.value.size){
-      theSameTime += 1
-    }else if(resultJustification.value.size > resultSize){
+    if(resultJustification.value.equals(justifications)){
       theSameTime = 0
       resultSize = resultJustification.value.size
     }
+//    resultJustification.value.foreach(j => println("size:"+j.size+"#####just"+j))
     //判断reduceRdd是否需要进一步迭代
-    if (!reduceRdd.isEmpty() && accum.value < 10000L && theSameTime < 6) {
-      println("rddInit:")
-      println(rddInit.sortBy(x=>x._1.toString()).collect().foreach(println))
-      println("reduceRdd:")
-      println(reduceRdd.sortBy(x=>x._1.toString()).collect().foreach(println))
+    if (reduceRdd.collect().size > 0 && accum.value < 1000L ) {
+//      println("rddInit:")
+//      println(rddInit.sortBy(x=>x._1.toString()).collect().foreach(println))
+//      println("reduceRdd:")
+//      println(reduceRdd.sortBy(x=>x._1.toString()).collect().foreach(println))
       findJustifications(reduceRdd)
     }else{
       resultJustification.value.foreach(j => println("#####just"+j))
@@ -207,6 +264,20 @@ object Justification_mapreduce {
     else{
       set1.foreach(triple => {
         if(!set2.exists(tri => tri.equals(triple))){
+          result = false
+        }
+      })
+    }
+    result
+  }
+  def includeSetTriples(set1:Set[Triple],set2:Set[Triple]): Boolean = {
+    var result:Boolean = true
+    if(set1.size < set2.size) {
+      result = false
+    }
+    else{
+      set2.foreach(triple => {
+        if(!set1.exists(tri => tri.equals(triple))){
           result = false
         }
       })
--- src/main/scala/graphx/Justification_v7.scala
+++ src/main/scala/graphx/Justification_v7.scala
@@ -11,6 +11,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection.mutable
 import scala.collection.mutable.{Map, Set, Stack}
+import scala.io.Source
 
 /**
   * Created by user on 2017/5/25.
@@ -53,15 +54,25 @@ object Justification_v7 {
       .setAppName("SimpleGraphX")
       .setMaster("local")
     var sc = new SparkContext(conf)
-    try{
+    try {
       CassandraDB.connect()
-            var datarows = CassandraDB.session.execute("select * from mrjks.justifications limit 50;")
-            import scala.collection.JavaConversions._
-            for (row <- datarows) {
-              var triple = new utils.Triple(row.getLong("sub"),row.getLong("pre"),row.getLong("obj"),false)
-              dataRows.add(triple)
-            }
-//      dataRows.add(new utils.Triple(579225L,0,34359751031L,false))
+      var tripleMessage:Set[String] = Set()
+//      var datarows = CassandraDB.session.execute("select * from mrjks.justifications limit 5000;")
+//      import scala.collection.JavaConversions._
+//      for (row <- datarows) {
+//        var triple = new utils.Triple(row.getLong("sub"), row.getLong("pre"), row.getLong("obj"), false)
+//        dataRows.add(triple)
+//      }
+      var fileSource = Source.fromFile("inputs.txt")
+      for(line <- fileSource.getLines) {
+        var strs = line.split("\t")
+        sub = strs(0).toLong
+        pre = strs(1).toLong
+        obj = strs(2).toLong
+        var triple = new utils.Triple(sub,pre,obj,false)
+        dataRows.add(triple)
+      }
+
       for (data <- dataRows) {
         //        --subject 8589955511 --predicate 0 --object 30064810538
         println("###########--subject " + data.subject + " --predicate " + data.predicate + " --object " + data._object)
@@ -70,8 +81,89 @@ object Justification_v7 {
         start = System.nanoTime()
         //init vertext and edge
         initVertexAndEdge(new utils.Triple(target._1, target._2, target._3, false))
-        println("最大深度:"+maxDepth)
+        println("最大深度:" + maxDepth)
+
+        //判断是否有辩解
+        if (vertexMap.size == 1 && edgeMap.isEmpty) {
+          println("此三元组没有辩解")
+          end = System.nanoTime()
+          var results = Array(target.toString(), "0", ((end - start) / 1000000).toString)
+          resultSet.add((results, Array()))
+        } else {
+          //构造vertexRDD和edgeRDD
+          searchMap = vertexMap.map((x: ((Long, Long, Long), Long)) => (x._2, x._1))
+          vertexRDD = sc.parallelize(searchMap.map((m: (Long, (Long, Long, Long))) => (m._1, true)).toSeq)
+          edgeRDD = sc.parallelize(edgeSet.toSeq)
+          targetNum = vertexMap(target)
+          //构造图Graph[VD,ED]
+          graph = Graph(vertexRDD, edgeRDD)
+          //Pregel API
+          import scala.collection.mutable.Map
+
+          val graphOneStep = graph.aggregateMessages[Map[Long, Set[Long]]](
+            e => {
+              e.sendToDst(Map(e.attr._1 -> Set(e.srcId)))
+            }, (a, b) => {
+              var map1 = a
+              var map2 = b
+              for (key1 <- map2.keySet) {
+                if (map1.contains(key1)) {
+                  map1(key1) ++= map2(key1)
+                } else {
+                  map1 ++= Map(key1 -> map2(key1))
+                }
+              }
+              a
+            }
+          ).collectAsMap()
+          val oneStepMaps = sc.broadcast(graphOneStep)
+          var initSet = Set(targetNum)
+          var justifications: Array[Set[Long]] = Array(initSet)
+          var lastjust: Array[Set[Long]] = Array()
+          var step:Int = 0
+          //求取辩解
+          var resultRDD = sc.makeRDD(Seq(initSet))
+          //          resultRDD.map(x=>(x,x.size)).reduce((x,y)=>(x._1,x._2+y._2))._2 > resultSize
+          while (step < maxDepth) {
+            step += 1
+            justifications = resultRDD.flatMap(x => {
+              var results: Set[Set[Long]] = Set(x)
+              //find one step justifications
+              var tracingEntries: Map[Long, Set[Long]] = Map()
+              x.foreach(vertex => {
+                if (oneStepMaps.value.contains(vertex)) {
+                  tracingEntries = oneStepMaps.value(vertex)
+                  var temps: Set[Set[Long]] = results.clone()
+                  for (temp <- temps) {
+                    if (temp.contains(vertex)) {
+                      for (tracing <- tracingEntries.keySet) {
+                        var tracedTriples: Set[Long] = tracingEntries(tracing)
+                        var _temp: Set[Long] = temp.clone()
+                        _temp.remove(vertex)
+                        _temp = _temp ++ tracedTriples
+                        results.add(_temp)
+                      }
+                      results.remove(temp)
+                    }
+                  }
+                }
+              })
+              results
+            }).collect()
+            resultRDD = sc.makeRDD(justifications)
+          }
+          end = System.nanoTime()
+          var results = Array(target.toString(),(justifications.size).toString,((end-start)/1000000).toString)
+          resultSet.add((results,justifications))
+        }
+        var str = data.subject +"\t"+ data.predicate+"\t"+data._object+"\t"+maxDepth++"\t"+vertexMap.size+"\t"+edgeMap.size
+        println(str)
+        //        tripleMessage.add(str)
       }
+      writeToResults(resultSet)
+//      var writer = new PrintWriter(new File("D:\\tripleMessage.txt"))
+//      tripleMessage.foreach(writer.println(_))
+//      writer.close()
     }catch {
       case ex:Exception => println(ex.getMessage())
     }
@@ -127,21 +219,21 @@ object Justification_v7 {
           var verterFromNumber = getVertexNumber((singleTriple.subject,singleTriple.predicate,singleTriple._object))
           //检测vertexFromNumber是否已经存在栈中，存在说明有环
           if(vertexStack.exists(v => v == verterFromNumber)){
-            println("发现环路："+verterFromNumber+"->")
-            vertexStack.foreach(v =>print(v+"->"))
-            println()
-          }
-
-          //定点不可指向自己进行循环
-          if(verterFromNumber != vertexToNumber){
-            var edge = new Edge(verterFromNumber,vertexToNumber,relation)
-            var edge_ = new Edge(vertexToNumber,verterFromNumber,relation)
-            //            if(vertexMap.contains(verterFromNumber))
-            if(!edgeSet.exists(e => e.srcId == vertexToNumber && e.dstId == verterFromNumber) && !edgeSet.contains(edge)) {
-              //            if(!edgeSet.contains(edge)) {
-//              println(edge.toString())
-              edgeSet.add(edge)
-              initVertexAndEdge(singleTriple)
+//            println("发现环路："+verterFromNumber+"->")
+//            vertexStack.foreach(v =>print(v+"->"))
+//            println()
+          }else{
+            //定点不可指向自己进行循环
+            if(verterFromNumber != vertexToNumber){
+              var edge = new Edge(verterFromNumber,vertexToNumber,relation)
+              var edge_ = new Edge(vertexToNumber,verterFromNumber,relation)
+              //            if(vertexMap.contains(verterFromNumber))
+              if(!edgeSet.exists(e => e.srcId == vertexToNumber && e.dstId == verterFromNumber) && !edgeSet.contains(edge)) {
+                //            if(!edgeSet.contains(edge)) {
+                //              println(edge.toString())
+                edgeSet.add(edge)
+                initVertexAndEdge(singleTriple)
+              }
             }
           }
         }
@@ -182,18 +274,17 @@ object Justification_v7 {
       result
     }
   }
-  def compareSets(set1:Array[Set[Long]],set2:Array[Set[Long]]): Boolean = {
-    var result:Boolean = true
-    if(set1.length != set2.length) {
-      result = false
-    }
-    else{
-      set1.foreach(setitem => {
-        if(!set2.contains(setitem)){
-          result = false
-        }
-      })
-    }
-    result
+
+  /**
+    * print result
+    * @param results
+    */
+  def writeToResults(results:Set[(Array[String],Array[Set[Long]])]) =  {
+    var writer = new PrintWriter(new File("resultsNewJust.txt"))
+    results.foreach(r => {
+      writer.println(r._1.apply(0)+"\t"+r._1.apply(1)+"\t"+r._1.apply(2))
+      r._2.foreach(println(_))
+    })
+    writer.close()
   }
 }
